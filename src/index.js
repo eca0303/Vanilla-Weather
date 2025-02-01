2f04116691e9e95514a6313593a9bd25fabe5aa5
eca0303 <emma.c.dsilva@gmail.com>
2025-02-02 00:58:52 +1100

    Added html and js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 function refreshWeather(response) {
-    let temperatureElement = documemnt.querySelector("#temperature")
-    let temperature = responese.data.temperature.current;
-    
-    cityElement.innerHTML = serachInput.value;
-    temperatureElement.innerHTML = Math.round(temperature);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let humidityElement = document.querySelector("#humidity");
+    let descriptionElement = document.querySelector("#description");
+    let windSpeedElement = document.querySelector("#wind-speed");
 
+
+    let temperature = response.data.temperature.current;
+    let humidity = response.data.temperature.humidity;
+    let description = response.data.condition.description;
+    let windSpeed = response.data.wind.speed;
+    let city = response.data.city;
+
+
+    cityElement.innerHTML = city;
+    temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+    humidityElement.innerHTML = `${humidity}%`;
+    descriptionElement.innerHTML = description;
+    windSpeedElement.innerHTML = `${Math.round(windSpeed)} km/h`;
 }
 
 function searchCity(city) {
-    let apikey = "370ab147d13obce21d786caff668ftb9";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
-axios.get(apiUrl).then(refreshWeather)
+    let apiKey = "370ab147d13obce21d786caff668ftb9";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
 }
 
-function handleSearchSumbit(event) {
+function handleSearchSubmit(event) {
     event.preventDefault();
-    let serachInput = document.querySelector("#search-form-input");
-    console.log(serachInput.value);
-    let cityElement = document.querySelector("#city")
-    
-    searchCity(serachInput.value)
-    //call the API 
-    //Search for the 
+    let searchInput = document.querySelector("#search-form-input");
 }
 
-let serachFormElement = document.querySelector("#search-form");
-serachFormElement.addEventListener("sumbit", handleSearchSumbit)
-
-searchCity(Sydney)
+searchCity("Sydney");
