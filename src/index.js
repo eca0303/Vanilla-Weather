c13145e657325b02f2d7c1c7712b6bb1c20d00fc
eca0303 <emma.c.dsilva@gmail.com>
2025-02-02 16:01:34 +1100

    Corrected JS and Html for the API integration for forecast

--- src/index.js
+++ src/index.js
@@ -1,34 +1,56 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let humidityElement = document.querySelector("#humidity");
-    let descriptionElement = document.querySelector("#description");
-    let windSpeedElement = document.querySelector("#wind-speed");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
+}
 
-    let temperature = response.data.temperature.current;
-    let humidity = response.data.temperature.humidity;
-    let description = response.data.condition.description;
-    let windSpeed = response.data.wind.speed;
-    let city = response.data.city;
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-
-    cityElement.innerHTML = city;
-    temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
-    humidityElement.innerHTML = `${humidity}%`;
-    descriptionElement.innerHTML = description;
-    windSpeedElement.innerHTML = `${Math.round(windSpeed)} km/h`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
-    let apiKey = "370ab147d13obce21d786caff668ftb9";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
+  let apiKey = "370ab147d13obce21d786caff668ftb9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+function handleSubmitSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSubmitSearch);
 
 searchCity("Sydney");
